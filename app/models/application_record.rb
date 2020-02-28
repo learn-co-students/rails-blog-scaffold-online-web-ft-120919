d417f9ff155def7cc88459f5fd4bc3c4f790a1f1
github username <meliajm@gmail.com>
2020-02-27 19:11:15 -0800

    Done.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
+# rails g scaffold Apartment address:string price:float description:text image_url:string --no-test-framework
+# rails g scaffold Post title:string description:text --no-test-framework
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
